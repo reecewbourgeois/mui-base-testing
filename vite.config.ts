26b894a80d915203f54036826b333559e1b71fc4
Reece Bourgeois <25267927+reecewbourgeois@users.noreply.github.com>
2023-10-17 21:40:04 -0500

    update & remove packages

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@ import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import eslintPlugin from '@nabla/vite-plugin-eslint';
 import svgr from 'vite-plugin-svgr';
-import sassDts from 'vite-plugin-sass-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), svgr(), eslintPlugin(), sassDts()],
+    plugins: [react(), svgr(), eslintPlugin()],
     server: {
         port: 3000,
     },
